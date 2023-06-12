71974bf62085e1ebffe6a891f46d626072a44eb3
halu <hlu07472@gmail.com>
2023-06-12 22:04:49 +0900

    Initial Commit

---
+++ SwiftConcurrencyDemos/SwiftConcurrencyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyDemosApp.swift
+//  SwiftConcurrencyDemos
+//
+//  Created by Lu Haoyu on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
